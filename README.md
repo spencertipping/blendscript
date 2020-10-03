46f808a21ff194a94a856767e848a95f816f84c1
Spencer Tipping <spencer.tipping@gmail.com>
2020-10-03 10:25:35 -0600

    WIP some initial stuff

---
+++ README.md
@@ -0,0 +1,4 @@
+# BlendScript: concise Blender scripting for CAD
+I wrote BlendScript to make it easier to do CAD modeling in Blender.
+
+**TODO:** more of a readme
---
+++ blendscript.py
@@ -0,0 +1,15 @@
+"""
+Blender scripting language with a focus on brevity.
+
+The goal is to make it possible to drive Blender using just a script, and using
+fewer keystrokes than you'd use against the normal UI. This library is oriented
+towards CAD, engineering, and precision editing.
+
+BlendScript inherits a lot from the Blender scripting API; for instance, it's
+imperative and passes state through the view context.
+"""
+
+def parse(x): pass
+
+def blend(source):
+  parse(source)
