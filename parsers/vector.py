06c711a472ee9e326b83520e92b144a29c096c7c
Spencer Tipping <spencer.tipping@gmail.com>
2020-10-04 11:16:54 -0600

    WIP figuring out what kind of operations we want; I think the right approach is to build stuff up around affine matrices

--- parsers/vector.py
+++ parsers/vector.py
@@ -1,8 +1,7 @@
 """
-Vector specifications.
+Vector specification.
 
-Vectors are specified as a series of operations applied to an initial value, in
-this case always the origin.
+
 """
 
 from .basic       import *
@@ -19,18 +18,20 @@ p_vecatom = alt(pmap(lambda xs: xs[1], seq(re(b'\['), p_vector, re(b'\]'))),
                 pmap(lambda x: Vector((x, x, x)), p_float))
 
 # Numeric broadcasting
-vector_op.append(
-  pmap(lambda x: lambda v: v + Vector((x, x, x)), p_vecatom)
-)
+vector_op.append(pmap(lambda x: lambda v: v + x, p_vecatom))
 
 # Single-component selection
 vector_op.append(
   pmap(lambda v1: lambda v: v + Vector((v1[1][0], 0, 0)), seq(re(b'x'), p_vecatom)),
   pmap(lambda v1: lambda v: v + Vector((0, v1[1][1], 0)), seq(re(b'y'), p_vecatom)),
-  pmap(lambda v1: lambda v: v + Vector((0, 0, v1[1][2])), seq(re(b'z'), p_vecatom)),
+  pmap(lambda v1: lambda v: v + Vector((0, 0, v1[1][2])), seq(re(b'z'), p_vecatom)))
+
+# Inversions
+vector_op.append(
   pmap(lambda v1: lambda v: v - v1[1], seq(re(b'-'), p_vecatom)),
   pmap(lambda v1: lambda v: v + Vector((1 / (v1[1][0] or 1),
                                         1 / (v1[1][1] or 1),
                                         1 / (v1[1][2] or 1))),
-       seq(re(b'/'), p_vecatom))
-)
+       seq(re(b'/'), p_vecatom)))
+
+# Mesh references
