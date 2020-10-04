a65c72a8c20bae47adb81adc702f4bff30073228
Spencer Tipping <spencer.tipping@gmail.com>
2020-10-04 08:14:24 -0600

    Fixed vector precedence

--- parsers/vector.py
+++ parsers/vector.py
@@ -15,21 +15,22 @@ vector_op = alt()
 p_vector  = pmap(lambda ops: reduce(lambda v, f: f(v), ops, Vector((0, 0, 0))),
                  rep(vector_op, min=1))
 
+p_vecatom = alt(pmap(lambda xs: xs[1], seq(re(b'\['), p_vector, re(b'\]'))),
+                pmap(lambda x: Vector((x, x, x)), p_float))
+
 # Numeric broadcasting
 vector_op.append(
-  pmap(lambda x: lambda v: v + Vector((x, x, x)), p_float)
+  pmap(lambda x: lambda v: v + Vector((x, x, x)), p_vecatom)
 )
 
 # Single-component selection
-# TODO: some precedence to make x2y1 work
-
 vector_op.append(
-  pmap(lambda v1: lambda v: v + Vector((v1[1][0], 0, 0)), seq(re(b'x'), p_vector)),
-  pmap(lambda v1: lambda v: v + Vector((0, v1[1][1], 0)), seq(re(b'y'), p_vector)),
-  pmap(lambda v1: lambda v: v + Vector((0, 0, v1[1][2])), seq(re(b'z'), p_vector)),
-  pmap(lambda v1: lambda v: v - v1[1], seq(re(b'-'), p_vector)),
+  pmap(lambda v1: lambda v: v + Vector((v1[1][0], 0, 0)), seq(re(b'x'), p_vecatom)),
+  pmap(lambda v1: lambda v: v + Vector((0, v1[1][1], 0)), seq(re(b'y'), p_vecatom)),
+  pmap(lambda v1: lambda v: v + Vector((0, 0, v1[1][2])), seq(re(b'z'), p_vecatom)),
+  pmap(lambda v1: lambda v: v - v1[1], seq(re(b'-'), p_vecatom)),
   pmap(lambda v1: lambda v: v + Vector((1 / (v1[1][0] or 1),
                                         1 / (v1[1][1] or 1),
                                         1 / (v1[1][2] or 1))),
-       seq(re(b'/'), p_vector))
+       seq(re(b'/'), p_vecatom))
 )
