ff1b90878ee0adaa50e1dbe06c4b779d4b9a4a86
Spencer Tipping <spencer.tipping@gmail.com>
2020-10-04 06:15:49 -0600

    Much-improved blender data access

--- parsers/basic.py
+++ parsers/basic.py
@@ -3,6 +3,7 @@ Basic building blocks for grammars.
 """
 
 import bpy
+
 from .combinators import *
 
 p_word  = re(br'\w+')
@@ -10,9 +11,17 @@ p_int   = pmap(int,   re(br'-?\d+'))
 p_float = pmap(float, re(br'-?\d*\.\d(?:\d*(?:[eE][+-]?\d+)?)?'
                          + br'|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?'))
 
-p_object = pmap(lambda s: bpy.data.objects[s], p_word)
-p_mesh   = pmap(lambda s: bpy.data.meshes[s],  p_word)
-
 p_comment    = re(br'#.*\n?')
 p_whitespace = re(br'\s+')
 p_ignore     = rep(alt(p_whitespace, p_comment), min=1)
+
+def pmember_of(h, p):
+  return pif(lambda x: x is not None,
+             pmap(lambda s: h.get(s.decode(), None), p))
+
+p_object    = pmember_of(bpy.data.objects,    p_word)
+p_armature  = pmember_of(bpy.data.armatures,  p_word)
+p_mesh      = pmember_of(bpy.data.meshes,     p_word)
+p_scene     = pmember_of(bpy.data.scenes,     p_word)
+p_workspace = pmember_of(bpy.data.workspaces, p_word)
+p_world     = pmember_of(bpy.data.worlds,     p_word)
