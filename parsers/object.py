f7a824130abbe713fca7c5fcb9ee0f8bc1b5460f
Spencer Tipping <spencer.tipping@gmail.com>
2020-10-04 13:45:13 -0600

    Rethinking this stuff, focusing on the object side rather than the vector side for a moment

--- parsers/object.py
+++ parsers/object.py
@@ -1,3 +1,11 @@
 """
+Objects and basic object interactions.
 
+I think it's fine if we always start with a single point and proceed using one
+of a few operations:
+
+1. Extrude along vector, expanding frontier
+2. Extrude along vector, replacing frontier
+3. Close current frontier with edge/face/both and jump by vector, starting a
+   new frontier
 """
