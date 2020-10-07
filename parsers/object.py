46e91438bfa397892cfb26120a0d64da06cd373c
Spencer Tipping <spencer.tipping@gmail.com>
2020-10-07 07:33:19 -0600

    A lot of stuff works, but we still get rejected faces for some reason. Duplicated front verts maybe?

--- parsers/object.py
+++ parsers/object.py
@@ -11,43 +11,14 @@ point and proceed inductively, with each step one of a few operations:
 5. Collapse frontier, connecting with edges
 6. Collapse frontier, connecting with edges+faces
 
-For example, a 1x1x1 cube would work like this:
+For example, a 1x1x1 cube works like this:
 
-1. Extrude+expand edge along X [frontier has two points]
-2. Extrude+expand face along Y [frontier has four points]
-3. Extrude+replace faces along Z [frontier has four points]
-4. Collapse with edges+face [frontier has one point]
-
-Frontiers need to encode adjacency, which can either be a list or a loop. Each
-point on the frontier can be tagged with one or more labels and selected by
-tag.
-
-We need different movement options: one for jumps (points), one for edges, and
-one for faces+edges. I think it's fine for us to assume that our object will
-end up being a shell of some sort -- i.e. no conspicuous holes. So we can
-eagerly connect stuff. Make new elements if any point has moved.
-
-Loops need to be preserved across certain types of expand-extrusions. The case
-above is simple enough, but what happens if the profile is more complex? Can we
-create two disjoint loops? Suppose we select even/odd points and extrude
-orthogonally. What would we expect to happen?
-"""
-
-"""
-Proposed extrusion rules.
-
-Map each point to its new position, which may be the same as its original
-position. If any point has moved, create new edges/faces/etc from it. The only
-thing we need to do is make sure we don't duplicate new elements.
-
-I think it's fine to say that points extrude to edges, edges extrude to faces.
-I don't think we need to model solids as extrusions of faces.
-
-Loops are inferred by traversing the edge graph. We don't rely on edge
-directed-ness or ordering for loops because extrusions don't preserve direction
-in the right way. For example, "extrude along X; extrude along Y" for a cube
-face would produce parallel edges in each direction; there's a diamond but no
-loop if you assume a DAG.
+> f = frontier()
+> m = f.extrude(Vector((1, 0, 0)), expand=True,  edges=True)
+>      .extrude(Vector((0, 1, 0)), expand=True,  edges=True, faces=True)
+>      .extrude(Vector((0, 0, 1)), expand=False, edges=True, faces=True)
+>      .collapse(edges=True, faces=True)
+>      .mesh('cube')
 """
 
 import bpy
@@ -76,7 +47,7 @@ class frontier:
     self.front[].
     """
 
-    self.tags = {'init': [0]}
+    self.tags = {'e': [0]}
     """
     The mapping of tags used to select subsets of the wavefront to operate on.
     Values are lists of indexes into self.front[].
@@ -90,7 +61,7 @@ class frontier:
     For now, queries are just tag names or `None` to select everything.
     """
     if query is None:
-      return xrange(len(self.front))
+      return range(len(self.front))
     else:
       return self.tags[query]
 
@@ -100,8 +71,13 @@ class frontier:
     """
     m = bpy.data.meshes.new(name)
     m.from_pydata(self.vertices, self.edges, self.faces)
+    for v in m.vertices: v.normal = Vector((0, 0, 1))
+
+    if m.validate(verbose=True):
+      raise Exception(
+        f'frontier.mesh(): {name} failed to validate (see console trace)')
+
     m.update()
-    if not m.validate(verbose=True): raise f'mesh {m} failed to validate'
     return m
 
   def extrude(self, dv, query=None, tag_as=None,
@@ -126,28 +102,29 @@ class frontier:
     if expand:
       # Extrude each point by calculating its new location, adding it to the
       # mesh, and logging a new link between the original and the new one.
+      front_moves = {}
       for fi in self.select(query):
         vi = self.front[fi]
-        self.vertices.append(self.vertices[vi] + dv)
-        self.front.append(len(self.vertices) - 1)
+        self.front.append(self.vertex(self.vertices[vi] + dv))
         new_fi = len(self.front) - 1
         front_edits[new_fi] = vi
+        front_moves[fi] = new_fi
         self.links.append((fi, new_fi))
         if tag_as is not None: self.tags[tag_as].append(new_fi)
 
-        # Anyone who linked to the original should also link to the new point.
-        for (f1, f2) in self.links:
-          if f1 == fi: self.links.append((new_fi, f2))
-          if f2 == fi: self.links.append((f1, new_fi))
+      # If f1 and/or f2 refer to points that have moved, add a new edge to the
+      # destination.
+      for (f1, f2) in self.links:
+        if f1 in front_moves or f2 in front_moves:
+          self.links.append((front_moves.get(f1, f1), front_moves.get(f2, f2)))
 
     else:
       # Replace self.front[] entries with new extruded vertices. No new links
       # are created.
       for fi in self.select(query):
         vi = self.front[fi]
+        self.front[fi] = self.vertex(self.vertices[vi] + dv)
         front_edits[fi] = vi
-        self.vertices.append(self.vertices[vi] + dv)
-        self.front[fi] = len(self.vertices) - 1
         if tag_as is not None: self.tags[tag_as].append(fi)
 
     if edges: self.create_edges(front_edits)
@@ -164,14 +141,19 @@ class frontier:
     deterministically).
     """
     front_edits = {}
-    fis         = self.query(query)
-    tfi         = None
+    fis         = self.select(query)
 
-    # Any point in the target will do, so long as it's also in the query set.
-    for t in self.query(target): if t in fis: tfi = t; break
+    # Any point in the target will do so long as it's also in the query set.
+    tfi = None
+    for t in self.select(target):
+      if t in fis:
+        tfi = t
+        break
+
     if tfi is None:
-      raise "frontier.collapse(): target query must specify a point within "
-            "the wavefront being collapsed"
+      raise Exception(
+        "frontier.collapse(): target query must specify a point within "
+        "the wavefront being collapsed")
 
     # Create edit records for all points that aren't the target. Since we're
     # collapsing down to a smaller set, we hang onto the originals for
@@ -188,22 +170,58 @@ class frontier:
     # also need to rewrite self.links[] and self.tags[].
     new_front = []
     front_map = {}
-    for fi in xrange(len(self.front)):
+    for fi in range(len(self.front)):
       # "edit" == "remove" in this context, so exclude from the new front.
       if fi not in front_edits:
         new_front.append(fi)
         front_map[fi] = len(new_front) - 1
 
-    new_links = [(front_map[f1], front_map[f2])
-                 for (f1, f2) in self.links
-                 if front_map[f1] != front_map[f2]]
-    new_tags  = {}
-    for t in self.tags:
-      new_tags[t] = [front_map[fi] for fi in self.tags[t]]
-
     self.front = new_front
-    self.links = new_links
-    self.tags  = new_tags
+    self.links = [(front_map[f1], front_map[f2])
+                  for (f1, f2) in self.links
+                  if f1 in front_map and f2 in front_map
+                      and front_map[f1] != front_map[f2]]
+    self.tags  = dict([(t, [front_map[fi]
+                            for fi in self.tags[t]
+                            if fi in front_map])
+                       for t in self.tags])
+    return self
+
+  def vertex(self, v):
+    """
+    Adds a vertex to the geometry, returning its vi. If the vertex already
+    exists, you'll get the existing vi for it.
+    """
+    for i in range(len(self.vertices)):
+      if self.vertices[i] == v:
+        return i
+    self.vertices.append(v)
+    return len(self.vertices) - 1
+
+  def edge(self, vi1, vi2):
+    """
+    Adds an edge to the geometry, provided that the edge won't violate any
+    constraints (for instance, it can't already exist and its endpoints can't
+    be the same).
+    """
+    if vi1 == vi2: return self
+    for (v1, v2) in self.edges:
+      if v1 == vi1 and v2 == vi2 or v1 == vi2 and v2 == vi1:
+        return self
+
+    self.edges.append((vi1, vi2))
+    return self
+
+  def face(self, *vs):
+    """
+    Adds a face to the geometry, provided that the face won't violate any
+    constraints.
+    """
+    for i in range(len(vs)):
+      if vs[i] in vs[i:]:
+        return self
+
+    self.faces.append(tuple(vs))
     return self
 
   def create_edges(self, front_edits):
@@ -218,7 +236,12 @@ class frontier:
         # and that means we have both the old and new points available.
         # Non-new edges always correspond to links within the updated
         # wavefront, which is also covered by what is now self.front[].
-        self.edges.append((self.front[f1], self.front[f2]))
+        self.edge(self.front[f1], self.front[f2])
+
+    # Moving a point creates an edge even if there is no link between the old
+    # and new locations.
+    for fi in front_edits:
+      self.edge(front_edits[fi], self.front[fi])
 
   def create_faces(self, front_edits):
     # Faces are a lot like edges, except that we also need to consider the
@@ -233,6 +256,6 @@ class frontier:
       v10 = front_edits.get(f1, v1)
       v20 = front_edits.get(f2, v2)
 
-      if v10 != v1 and v20 != v2: self.faces.append((v10, v1, v2, v20))
-      elif v10 != v1:             self.faces.append((v10, v1, v2))
-      elif v20 != v2:             self.faces.append(     (v1, v2, v20))
+      if v10 != v1 and v20 != v2: self.face(v10, v1, v2, v20)
+      elif v10 != v1:             self.face(v10, v1, v2)
+      elif v20 != v2:             self.face     (v1, v2, v20)
