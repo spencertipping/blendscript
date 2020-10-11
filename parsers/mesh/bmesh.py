111e341b409361edf96aa88fa6bef59528d5aed3
Spencer Tipping <spencer.tipping@gmail.com>
2020-10-11 12:37:37 -0600

    WIP managed bmesh object

--- parsers/mesh/bmesh.py
+++ parsers/mesh/bmesh.py
@@ -41,8 +41,64 @@ class bmesh_and_selection:
   """
   A bmesh object that keeps track of the vertex, edge, and face selections
   resulting from each operation.
+
+  Each function takes a "q" argument to select its input elements. Functions
+  that return results allow you to store those results by specifying "r". In
+  some cases the default is to replace the current selection with the result;
+  for instance, extrusions. When that's true the default for "r" is "_".
   """
   def __init__(self, bmesh):
-    self.bmesh      = bmesh
-    self.selections = {}
-    self.last       = None
+    self.bmesh   = bmesh
+    self.subsets = {}
+
+  def select(self, q):
+    """
+    Evaluates the selection query, returning the result. Options are:
+
+    1. None: every object
+    2. "x": self.subsets[x]
+    3. ['*', q1, q2, ...]: intersection of queries
+    4. ['+', q1, q2, ...]: union of queries
+    5. ['b', v1, v2]: box-select between vertices
+    """
+    vs = self.bmesh.verts
+    es = self.bmesh.edges
+    fs = self.bmesh.faces
+
+    if q is None:            return vs[:] + es[:] + fs[:]
+    elif isinstance(q, str): return self.subsets[q]
+    elif isinstance(q, list):
+      c = q[0]
+      if q == '*': ...
+
+
+  def store(self, r, xs):
+    """
+    Stores the specified result set into a subset, ignoring if r == None.
+    """
+    if r is not None: self.subsets[r] = xs
+    return self
+
+  def context_fill(self, q=None, r=None):
+    ...
+
+  def extrude_faces(self, q, r='_'):
+    ...
+
+  def extrude_edges(self, q, r='_'):
+    ...
+
+  def extrude_verts(self, q, r='_'):
+    ...
+
+  def transform(self, q, m):
+    ...
+
+  def duplicate(self, q, r):
+    ...
+
+  def spin(self, q, r):
+    ...
+
+  def bridge_loops(self, q, r):
+    ...
