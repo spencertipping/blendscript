d017431205cf30fa1c56a79b28ee0ee865fee98c
Spencer Tipping <spencer.tipping@gmail.com>
2020-10-19 19:16:37 -0600

    OK, figured out how armatures work

--- blender/armature.py
+++ blender/armature.py
@@ -4,14 +4,42 @@ Blender armature and bone support.
 
 from ..compatibility import *
 
-from ..compiler.types import *
-from ..compiler.val   import *
-from .blender_objects import *
-from .blendermath     import *
+from ..compiler.types      import *
+from ..compiler.val        import *
+from ..runtime.blendermath import *
+
+from .blender_objects      import *
 
 
 try:
   import bpy
+
+  # example armature python:
+
+  '''
+  arm     = bpy.data.armatures.new('pig')
+  arm_obj = bpy.data.objects.new('armature', arm)
+  bpy.context.scene.collection.objects.link(arm_obj)
+
+  bpy.context.view_layer.objects.active = arm_obj
+  bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+
+  b1 = arm_obj.data.edit_bones.new('bone1')
+  b1.head = (1, 2, 3)
+  b1.tail = (4, 5, 6)
+
+  b2 = arm_obj.data.edit_bones.new('bone2')
+  b2.head = (4, 5, 6)
+  b2.tail = (9, 8, 7)
+
+  b2.parent = b1
+
+  bpy.ops.object.mode_set(mode='POSE')
+
+  bpy.data.objects['bar'].parent = arm_obj
+  bpy.data.objects['bar'].parent_bone = 'bone2'
+  bpy.data.objects['bar'].parent_type = 'BONE'
+  '''
 
 except ModuleNotFoundError:
   blender_not_found()
